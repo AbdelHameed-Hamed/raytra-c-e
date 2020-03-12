c7f6943cc9f3dd6568a756d1808051cc29bdf259
AbdelHameed Hamed <Abdelhameed.h.h@gmail.com>
2020-03-12 15:02:32 +0200

    Refined some of the Vec3 and Ray functions.

--- ray.c
+++ ray.c
@@ -1,22 +1,33 @@
 #include "vec3.c"
 
 struct IRay {
-	Vec3 A;
-	Vec3 B;
+	Vec3 a;
+	Vec3 b;
 };
 
 typedef struct IRay Ray;
 
-static Ray ray_init() { return (Ray){ vec3_init(), vec3_init() }; }
+static Ray
+ray_init() { return (Ray){ (Vec3){ 0.0f }, (Vec3){ 0.0f } }; }
 
-static Ray ray_set(Ray self, const Vec3 a, const Vec3 b) {
+static Ray
+ray_set(Ray self, const Vec3 a, const Vec3 b) {
 	memcpy(&self, &(Ray){a, b}, sizeof(Ray));
 
 	return self;
 }
 
-static Vec3 ray_origin(const Ray self) { return self.A; }
+static Vec3
+ray_origin(const Ray self) {
+	return self.a;
+}
 
-static Vec3 ray_direction(const Ray self) { return self.B; }
+static Vec3
+ray_direction(const Ray self) {
+	return self.b;
+}
 
-static Vec3 ray_point_at_parameter(Ray self, float t) { return self.A + (self.B * t); }
+static Vec3
+ray_point_at_parameter(Ray self, float t) {
+	return self.a + t * self.b;
+}
--- vec3.c
+++ vec3.c
@@ -1,58 +1,50 @@
 #include <math.h>
 #include <string.h>
 
-typedef float IVec3 __attribute__((ext_vector_type(4)));
+typedef float IVec3 __attribute__((ext_vector_type(3)));
 
 typedef IVec3 Vec3;
 
-static Vec3 vec3_init() { return (Vec3){ 0.0f }; }
-
-static float vec3_dot(const Vec3 lhs, const Vec3 rhs) {
-	float res = lhs[0] * rhs[0] +
-				lhs[1] * rhs[1] +
-				lhs[2] * rhs[2];
+static float
+vec3_dot(const Vec3 lhs, const Vec3 rhs) {
+	Vec3 temp = lhs * rhs;
+	float res = temp[0] + temp[1] + temp[2];
 
 	return res;
 }
 
-static Vec3 vec3_cross(const Vec3 lhs, const Vec3 rhs) {
-	Vec3 res = vec3_init();
-	res[0] = (lhs[1] * rhs[2]) - (lhs[2] * rhs[1]);
-	res[1] = (lhs[2] * rhs[0]) - (lhs[0] * rhs[2]);
-	res[2] = (lhs[0] * rhs[1]) - (lhs[1] * rhs[0]);
-
-	return res;
+static Vec3
+vec3_cross(const Vec3 lhs, const Vec3 rhs) { 
+	return lhs.yzx * rhs.zxy - lhs.zxy * rhs.yzx;
 }
 
-static Vec3 vec3_cross_inplace(Vec3 lhs, const Vec3 rhs) {
-	lhs[0] = (lhs[1] * rhs[2]) - (lhs[2] * rhs[1]);
-	lhs[1] = (lhs[2] * rhs[0]) - (lhs[0] * rhs[2]);
-	lhs[2] = (lhs[0] * rhs[1]) - (lhs[1] * rhs[0]);
+static Vec3
+vec3_cross_inplace(Vec3 lhs, const Vec3 rhs) {
+	lhs = lhs.yzx * rhs.zxy - lhs.zxy * rhs.yzx;
 
 	return lhs;
 }
 
-static float vec3_mag_sqr(const Vec3 self) {
-	return self[0] * self[0] +
-		   self[1] * self[1] +
-		   self[2] * self[2];
+static float
+vec3_mag_sqr(const Vec3 self) {
+	return vec3_dot(self, self);
 }
 
-static float vec3_mag(const Vec3 self) {
+static float
+vec3_mag(const Vec3 self) {
 	return sqrtf(vec3_mag_sqr(self));
 }
 
-static Vec3 vec3_make_unit(Vec3 self) {
+static Vec3
+vec3_make_unit(Vec3 self) {
 	float mag = vec3_mag(self);
-
-	self.xyz /= mag;
-	self.xyz /= mag;
 	self.xyz /= mag;
 
 	return self;
 }
 
-static Vec3 vec3_unit(const Vec3 self) {
+static Vec3
+vec3_unit(const Vec3 self) {
 	float mag = vec3_mag(self);
 	return self / mag;
 }
